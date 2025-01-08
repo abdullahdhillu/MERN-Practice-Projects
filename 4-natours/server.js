e82c5901bdff2e7095b543f1e5fd0ce81ac243cd
Abdullah Altaf <abdullahdhillu99@gmail.com>
2025-01-08 10:25:56 -0800

    Update server.js

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -9,7 +9,7 @@ mongoose.connect(DB).then(() => {
 }).catch(err => {
   console.log('DB connection failed:', err.message);
 });
-console.log("Hello");
+console.log("Hello Abdullah Dhillon");
 
 
 // console.log(process.env.NODE_ENV);
