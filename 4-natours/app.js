b67457b8dabd9f43ab7d60e9475e7c9c26dd003f
Abdullah Altaf <abdullahdhillu99@gmail.com>
2025-01-18 18:19:38 -0800

    Minor changes

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -3,6 +3,8 @@ const userRouter = require("./routes/userRoutes");
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const AppError = require("./utilities/appError");
+const globalErrorHandler = require("./controllers/globalErrorHandler");
 // console.log(process.env);
 // 2: Middlewares
 if (process.env.NODE_ENV === "development") {
@@ -21,21 +23,7 @@ app.use(express.static(`${__dirname}/public`)); // serve static files from publi
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
 app.all("*", (req, res, next) => {
-  // res.status(404).json({
-  //   status: "fail",
-  //   message: `Cannot find ${req.originalUrl} on this server!`,
-  // });
-  const err = new Error(`Cannot find ${req.originalUrl} on this server!`);
-  err.status = "fail";
-  err.statusCode = 404;
-  next(err);
+  next(new AppError(`Cannot find ${req.originalUrl} on this server!`, 404));
 });
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || "error";
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-});
+app.use(globalErrorHandler);
 module.exports = app;
