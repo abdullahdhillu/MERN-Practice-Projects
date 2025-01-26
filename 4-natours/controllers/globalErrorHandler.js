c4495423bc9180115d45803b75f053d7ea98df5f
Abdullah Altaf <abdullahdhillu99@gmail.com>
2025-01-26 10:43:23 -0800

    handling synchronous erros

--- 4-natours/controllers/globalErrorHandler.js
+++ 4-natours/controllers/globalErrorHandler.js
@@ -3,7 +3,18 @@ const castErrorHandlerDB = (err) => {
   const message = `Invalid ${err.path}: ${err.value}`;
   return new AppError(message, 400);
 };
-
+const handleDuplicateFieldsDB = (err) => {
+  const value = JSON.stringify(err.keyValue.name);
+  console.log(value);
+  const message = `Duplicate key: ${value} , please change value`;
+  return new AppError(message, 400);
+};
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+  const message = errors.join(". ");
+  // console.log(errors);
+  return new AppError(message, 500);
+};
 const sendErrorDev = (err, req, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -30,14 +41,15 @@ module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
   if (process.env.NODE_ENV === "development") {
-    console.log(err.name);
+    // console.log(err.name);
     sendErrorDev(err, req, res);
   } else if (process.env.NODE_ENV === "production") {
     let error = err;
     console.log(error);
-    if (error.name === "CastError") {
-      error = castErrorHandlerDB(error);
-    }
+    if (error.name === "CastError") error = castErrorHandlerDB(error);
+    if (error.code == 11000) error = handleDuplicateFieldsDB(error);
+    if (error.name === "ValidationError")
+      error = handleValidationErrorDB(error);
     sendErrorProd(error, req, res);
   }
   next();
--- 4-natours/server.js
+++ 4-natours/server.js
@@ -1,6 +1,10 @@
 const dotenv = require("dotenv"); // manages environment variables
 const mongoose = require("mongoose"); // MongoDB ORM
 dotenv.config({ path: "./config.env" }); // loads environment variables from .env file
+process.on("uncaughtException", (err) => {
+  console.log("UNCAUGHT EXCEPTION! Shutting down...");
+  console.log(err.message);
+});
 const app = require("./app.js"); // main application file
 // const DBS = "mongodb+srv://abdullahdhillon:0MkcvNawQyCg33rQ@cluster0.r9bve.mongodb.net/natours?retryWrites=true&w=majority&appName=Cluster0" // MongoDB connection string
 const DB = process.env.DATABASE.replace(
@@ -21,6 +25,13 @@ mongoose
 // creates a model from the schema
 
 const port = 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
+});
+process.on("unhandledRejection", () => {
+  console.log("UNHANDLED REJECTION! Shutting down...");
+});
+server.close(() => {
+  console.log("Server closed");
+  process.exit(1);
 });
