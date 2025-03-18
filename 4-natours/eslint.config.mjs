8641b2247e88d3d6002c28b00341cf31ac1de19b
Abdullah Altaf <abdullahdhillu99@gmail.com>
2025-03-17 23:16:06 -0700

    adding nodeMailer

--- 4-natours/eslint.config.mjs
+++ 4-natours/eslint.config.mjs
@@ -1,10 +1,16 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "commonjs", // Use CommonJS for Node.js
+      globals: {
+        ...globals.node, // Add Node.js globals (including `process`)
+      },
+    },
+  },
   pluginJs.configs.recommended,
 ];
---
+++ 4-natours/utilities/email.js
@@ -0,0 +1,19 @@
+const nodeMailer = require("nodemailer");
+const sendEmail = async (options) => {
+  const transporter = nodeMailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
+  const mailOptions = {
+    from: "Muhammad Abdullah <muhammadabdullah@gmail.com>",
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+  };
+  await transporter.sendMail(mailOptions);
+};
+module.exports = sendEmail;
